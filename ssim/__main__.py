47a9e4250450adc7d9c96017f81b14745acd5f93
Devasia Manuel <devasia1000@gmail.com>
2013-08-18 19:11:34 -0500

    fixed sorting bug

--- ssim/__main__.py
+++ ssim/__main__.py
@@ -13,24 +13,13 @@ def main():
     base_dir=args.base_dir
     compare_dir=args.comparison_dir
 
-    base_images=os.listdir(base_dir)
-    compare_images=os.listdir(compare_dir)
-
-    base_images.sort()
-    compare_images.sort()
-    
-    try:
-	#assert(len(base_images)==len(compare_images))
-	pass
-    except:
-	print "number of image files mismatch"
-	print base_dir, ": ", len(base_images)," ", compare_dir, ": ", len(compare_images)
-	sys.exit(0)
-
-    for i in range(len(base_images)):
-    	im1 = Image.open(args.base_dir+"/"+base_images[i])
-    	im2 = Image.open(args.comparison_dir+"/"+compare_images[i])
-    	print "SSIM for Image #", i, ": ", ssim.compute_ssim(im1, im2)
+    for i in range(100, 1000):
+        try:
+    	    im1 = Image.open(args.base_dir+str(i)+".png")
+    	    im2 = Image.open(args.comparison_dir+str(i)+".png")
+    	    print "SSIM for Image #", i, ": ", ssim.compute_ssim(im1, im2)
+        except:
+	    print "Error processing Image # ", i, ". File does not exist!"
     
 if __name__ == '__main__':
     main()
