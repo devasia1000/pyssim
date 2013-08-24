f3e2286098368a013dae6f0bb7dd69797c9febdc
Devasia Manuel <devasia1000@gmail.com>
2013-08-24 17:57:39 -0500

    corrected poor SSIM results by taking max of (n+1)th and nth SSIM

--- ssim/__main__.py
+++ ssim/__main__.py
@@ -13,11 +13,15 @@ def main():
     original_dir=args.original_dir
     compare_dir=args.comparison_dir
 
-    for i in range(1, 10001):
+    for i in range(4500, 10001):
 #        try:
     	    im1 = Image.open(args.original_dir+str(i)+".png")
-    	    im2 = Image.open(args.comparison_dir+str(i)+".png")
-    	    print "SSIM for Image #", i, ": ", ssim.compute_ssim(im1, im2)
+
+	    im2_same=Image.open(args.comparison_dir+str(i)+".png")
+	    im2_ahead=Image.open(args.comparison_dir+str(i+1)+".png")
+
+	    ssim_value=max(ssim.compute_ssim(im1, im2_same), ssim.compute_ssim(im1, im2_ahead))
+    	    print "SSIM for Image #", i, ": ", ssim_value
  #       except:
 #	    print "Error processing Image # ", i, ". File", args.base_dir+str(i)+str(".png"), "or", args.comparison_dir+str(i)+str(".png"), "does not exist!"
     
