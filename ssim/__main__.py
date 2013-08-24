bdce64b78606df5c79acce6623d72db3c04c0e14
Devasia Manuel <devasia1000@gmail.com>
2013-08-24 17:30:52 -0500

    renamed variable 'base_dir' to 'original_dir'

--- ssim/__main__.py
+++ ssim/__main__.py
@@ -6,16 +6,16 @@ import os, sys
 def main():
     parser = argparse.ArgumentParser(prog="pyssim",
                                      description="Compares two images using the SSIM metric")
-    parser.add_argument('base_dir')
+    parser.add_argument('original_dir')
     parser.add_argument('comparison_dir')
     args = parser.parse_args()
 
-    base_dir=args.base_dir
+    original_dir=args.original_dir
     compare_dir=args.comparison_dir
 
     for i in range(1, 10001):
 #        try:
-    	    im1 = Image.open(args.base_dir+str(i)+".png")
+    	    im1 = Image.open(args.original_dir+str(i)+".png")
     	    im2 = Image.open(args.comparison_dir+str(i)+".png")
     	    print "SSIM for Image #", i, ": ", ssim.compute_ssim(im1, im2)
  #       except:
